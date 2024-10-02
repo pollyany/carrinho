88ec85d6aa1b1e8a51a6fcaaeaf98572858f362b
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-02 14:19:53 -0300

    Criando página Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
- return (
-   <div>
+import { BsCartPlus } from "react-icons/bs";
 
-   </div>
- );
+export default function Home() {
+  return (
+    <div>
+      <main className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Produtos em alta
+        </h1>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+          <section className="w-full">
+            <img
+              className="w-full rounded-lg max-h-70 mb-2"
+              src="https://m.media-amazon.com/images/I/61CVih3UpdL.__AC_SY445_SX342_QL70_ML2_.jpg"
+              alt="Produto"
+            />
+            <p className="font-medium mt-1 mb-2">Airpos Pro</p>
+            <div className="flex gap-3 items-center">
+              <strong className="text-zinc-700/90">R$ 100</strong>
+              <button className="bg-zinc-900 p-1 rounded">
+                <BsCartPlus size={20} color="#fff" />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 }
