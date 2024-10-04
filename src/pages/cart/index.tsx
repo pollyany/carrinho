0c2c3d4ac31fce06bce98d94a7af645073709536
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-04 11:33:28 -0300

    Página carrinho de compras

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,32 @@
 export default function Cart() {
- return (
-   <div>
+  return (
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
 
-   </div>
- );
+      <section className="flex items-center justify-between border-b-2 border-gray-300">
+        <img
+          className="w-28"
+          src="https://m.media-amazon.com/images/I/61CVih3UpdL.__AC_SY445_SX342_QL70_ML2_.jpg"
+          alt="Produto"
+        />
+        <strong>Preço: R$ 1.000</strong>
+
+        <div className="flex items-center justify-center gap-3">
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            -
+          </button>
+          1
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            +
+          </button>
+        </div>
+
+        <strong className="float-right">
+          Subtotal: R$ 1.000
+        </strong>
+      </section>
+
+      <p className="font-bold mt-4">Total: R$ 1.000</p>
+    </div>
+  );
 }
